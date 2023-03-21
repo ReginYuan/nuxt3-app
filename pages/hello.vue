6cb4094479f741a7e46d41d4a25445d86660b486
ReginYuan <2496943860@qq.com>
2023-03-21 23:46:17 +0800

    feat:  封装请求接口

---
+++ pages/hello.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <!-- {{ message }} -->
+    </div>
+</template>
+  
+<script setup lang="ts">
+// const { message: any } = await $fetch('/api/hello')
+// console.log('message', message)
+</script>
