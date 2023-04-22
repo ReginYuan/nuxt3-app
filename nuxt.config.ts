66ea8c6a99d2d279c52ac98088d918dc54f6a7ba
ReginYuan <2496943860@qq.com>
2023-04-22 16:54:45 +0800

    feat: 修改对应链接地址

---
+++ nuxt.config.ts
@@ -0,0 +1,19 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    css: [
+        'assets/global.scss'
+    ],
+    vite: {
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData: '@import "~/assets/_variables.scss";',
+                }
+            }
+        }
+    },
+    modules: [
+        '@nuxtjs/tailwindcss',
+        "@huntersofbook/naive-ui-nuxt"
+    ]
+})
---
+++ pages/detail/[id].vue
@@ -0,0 +1,8 @@
+<template>
+  <h1>{{ title }}</h1>
+  <div v-html="content"></div>
+</template>
+<script setup lang="ts">
+const router = useRoute();
+const { title, content } = await $fetch(`/api/detail/${router.params.id}`);
+</script>
