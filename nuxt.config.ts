74c22f173a37925591848bee79619ccaec228091
ReginYuan <49477488+ReginYuan@users.noreply.github.com>
2024-04-01 16:55:26 +0800

    feat: 代码提交

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,10 +10,28 @@ export default defineNuxtConfig({
                     additionalData: '@import "~/assets/_variables.scss";',
                 }
             }
+        },
+        optimizeDeps: {
+            include:
+                process.env.NODE_ENV === 'development'
+                    ? ['naive-ui', 'vueuc', 'date-fns-tz/esm/formatInTimeZone']
+                    : []
         }
+    },
+    build: {
+        transpile:
+            process.env.NODE_ENV === 'production'
+                ? [
+                    'naive-ui',
+                    'vueuc',
+                    '@css-render/vue3-ssr',
+                    '@juggle/resize-observer'
+                ]
+                : ['@juggle/resize-observer']
     },
     modules: [
         '@nuxtjs/tailwindcss',
-        "@huntersofbook/naive-ui-nuxt"
+        "@huntersofbook/naive-ui-nuxt",
+        '@unocss/nuxt',
     ]
 })
---
+++ pages/count.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="p-4">
+    <!-- Couter:{{ counter }} -->
+    <div class="mt-2"></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+</style>
